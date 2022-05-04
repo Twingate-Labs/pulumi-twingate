8de166911fd3be2998a701713399507b6a66e138
Eran Kampf <eran@ekampf.com>
2022-05-03 19:28:34 -0700

    Send a version string to TF provider

--- provider/resources.go
+++ provider/resources.go
@@ -36,7 +36,7 @@ const (
 // Provider returns additional overlaid schema and metadata associated with the provider..
 func Provider() tfbridge.ProviderInfo {
 	// Instantiate the Terraform provider
-	p := shimv2.NewProvider(twingate.Provider("")) // TODO: Which version needs to be passed here?
+	p := shimv2.NewProvider(twingate.Provider("1.0-pulumi")) // TODO: Get real version
 
 	// Create a Pulumi provider mapping
 	prov := tfbridge.ProviderInfo{
