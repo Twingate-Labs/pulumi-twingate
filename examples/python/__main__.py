99b671ef028d6c924c5aec1b9911c042fc5c3a97
chenbishop <chen.bishop@gmail.com>
2023-02-23 14:24:32 +0000

    updating examples

--- examples/python/__main__.py
+++ examples/python/__main__.py
@@ -1,17 +1,26 @@
-import pulumi_twingate as twingate
 import pulumi
+import pulumi_twingate as tg
 
-remote_network = twingate.TwingateRemoteNetwork("python_pulumi_test", name="Python Pulumi Test")
+remote_network = tg.TwingateRemoteNetwork("test_network", name="Pulumi Test Network")
+service_account = tg.TwingateServiceAccount("ci_cd_account", name="CI CD Service")
+service_account_key = tg.TwingateServiceAccountKey("ci_cd_key", name="CI CD Key", service_account_id=service_account.id)
 
-service_account = twingate.TwingateServiceAccount("python_service", name="Python Service")
-service_account_key = twingate.TwingateServiceAccountKey("ci_cd_key", name="CI_CD_Key", service_account_id=service_account.id)
-
-twingate_resource = twingate.TwingateResource("python_example",
-    name="Python Pulumi Website",
-    address="www.pulumi.com",
-    remote_network_id=remote_network.id,
-    access= {"group_ids": ["R3JvdXA6NTcyNDU="], "service_account_ids": [service_account.id]}
-)
+# To see service_account_key, execute command `pulumi stack output --show-secrets`
+pulumi.export("service_account_key", service_account_key.token)
 
 
-pulumi.export("test", service_account_key.token)
+# Get group id by name
+def get_group_id(group_name):
+    group = tg.get_twingate_groups_output(name=group_name).groups[0]
+    return group.id
+
+
+group_id = get_group_id("Everyone")
+
+twingate_resource = tg.TwingateResource("test_resource",
+                                        name="Twingate Home Page",
+                                        address="www.twingate.com",
+                                        remote_network_id=remote_network.id,
+                                        access={"group_ids": [group_id],
+                                                "service_account_ids": [service_account.id]}
+                                        )
